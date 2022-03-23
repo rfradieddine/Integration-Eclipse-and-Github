a69a18503cd9aa7848db6873a5f4684bd436d7a5
Rafael Radieddine <89426621+rfradieddine@users.noreply.github.com>
2022-03-23 20:17:44 -0300

    Add arquivos

---
+++ SomarTeste/src/execSoma/SomarDoisNumeros.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package execSoma;
+
+/**
+ * @author Rafael Marques Radieddine
+ * Exemplo de integração com Github
+ */
+
+import java.util.Scanner;
+
+public class SomarDoisNumeros {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner (System.in);
+		
+		int x, y, total;
+		
+		System.out.println("Digite um numero:");
+		x = sc.nextInt();
+		System.out.println("Digite um numero:");
+		y = sc.nextInt();
+
+		total = y + x;
+		
+		System.out.printf("Resultado:" + total);
+		
+	}
+	
+
+}
